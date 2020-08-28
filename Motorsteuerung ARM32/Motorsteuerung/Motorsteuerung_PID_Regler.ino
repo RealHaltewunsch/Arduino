a480d23f6b7d6a30de5be69b8f6661e1755cf536
RealHaltewunsch <61188868+RealHaltewunsch@users.noreply.github.com>
2020-08-28 15:20:08 +0200

    Update Motorsteuerung_PID_Regler.ino
    -Stromreglung und Notbetrieb nicht mehr gleichzeitig möglich
    -Code gekürzt
    -doppelte Überprüfung verhindert

--- Motorsteuerung ARM32/Motorsteuerung/Motorsteuerung_PID_Regler.ino
+++ Motorsteuerung ARM32/Motorsteuerung/Motorsteuerung_PID_Regler.ino
@@ -1,25 +1,20 @@
-void Stromreglung_auslesen () {
-if (!digitalRead(Stromregelung_PIN) && !Notbetrieb) {
-  Stromregelung = true;
-  else {
-    Stromregelung = false;
-  }
-  digitalWrite(Stromregelung_PIN_Leuchte, Stromregelung);
-  if (Stromregelung) {
+void Stromreglung_auslesen() {
+  if (!digitalRead(Stromregelung_PIN) && !Notbetrieb) {
+    Stromregelung = true;
     StromPID.SetMode(AUTOMATIC);
+    else {
+      Stromregelung = false;
+      StromPID.SetMode(MANUAL);
+    }
+    digitalWrite(Stromregelung_PIN_Leuchte, Stromregelung);
   }
-  else {
-    StromPID.SetMode(MANUAL);
-  }
-  return;
-}
 
-void PID_Regler() { //aufruf nur wenn Stromregelung = true
-  //zuvor muss sichergestellt werden dass die Variable Input aktualisiert ist!!
-  Strom_messen();
-  Messwert_Gaspedal = map(Sollwert_relativ, GASPEDAL_MIN, GASPEDAL_MAX, 0, MAX_VALUE_CURRENT);
-  Messwert_Strom = Strom;
-  StromPID.Compute();  //das verändert die Variable Sollwert_PID
-  //Messwert_Strom, Sollwert_PID, Messwert_Gaspedal ANPASSEN
-  return;
-}
+  void PID_Regler() { //aufruf nur wenn Stromregelung = true
+    //zuvor muss sichergestellt werden dass die Variable Input aktualisiert ist!!
+    Strom_messen();
+    Messwert_Gaspedal = map(Sollwert_relativ, GASPEDAL_MIN, GASPEDAL_MAX, 0, MAX_VALUE_CURRENT);
+    Messwert_Strom = Strom;
+    StromPID.Compute();  //das verändert die Variable Sollwert_PID
+    //Messwert_Strom, Sollwert_PID, Messwert_Gaspedal ANPASSEN
+    return;
+  }
