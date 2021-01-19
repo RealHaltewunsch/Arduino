ce48d4479f813f79347c5913204401ca98cd138c
RealHaltewunsch <61188868+RealHaltewunsch@users.noreply.github.com>
2021-01-19 22:38:00 +0100

    Update Motorsteuerung_AVR.ino

--- Motorsteuerung/Motorsteuerung_AVR/Motorsteuerung_AVR.ino
+++ Motorsteuerung/Motorsteuerung_AVR/Motorsteuerung_AVR.ino
@@ -162,13 +162,13 @@ ADS1115_WE adc(I2C_ADDRESS);
 
 void setup() {
   Serial.begin(9600, SERIAL_8N1);  //Kommunikation mit Leistungselektronik
-  Serial.write(byte(0xE0));   //UART Mode, wenn 3 Sekunden kein Update erfolgt, Shutdown!
+  Serial.write(byte(0xE1));   //UART Mode, wenn 3 Sekunden kein Update erfolgt, Shutdown!
   Serial.write(byte(0x8A));    //Direction
   Serial.write(byte(0x00));    // STOP
   Serial.write(byte(0x84)); //accel limit
   Serial.write(byte(0x00)); //min
   Serial.write(byte(0x85)); //decel limit
-  Serial.write(byte(0x00)); //bisschen
+  Serial.write(byte(0x00)); //min
 
   Wire.begin();
   display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
