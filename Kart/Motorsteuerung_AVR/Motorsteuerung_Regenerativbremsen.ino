e145a2a4bd970eb3474554e7601c2088f0bbe0bb
RealHaltewunsch <61188868+RealHaltewunsch@users.noreply.github.com>
2021-02-10 13:03:40 +0100

    Logik angepasst

--- Kart/Motorsteuerung_AVR/Motorsteuerung_Regenerativbremsen.ino
+++ Kart/Motorsteuerung_AVR/Motorsteuerung_Regenerativbremsen.ino
@@ -1,16 +1,20 @@
 void Regenerativbremsen_Auslesen () {
-  bool Regenerativbremsen_alt = Regenerativbremsen;
+  int Strom_regen_hex_alt = Strom_regen_hex;
   Regenerativbremsen = !digitalRead(Regenerativbremsen_PIN);
 
-  if (Regenerativbremsen_alt != Regenerativbremsen) {
-    if (Regenerativbremsen) {
-      Strom_regen_hex = map(Regen_on, 0, 438, 0x00, 0x7F);
-    }
-    else {
-      Strom_regen_hex = map(Regen_off, 0, 438, 0x00, 0x7F);
-    }
+  if (Regenerativbremsen) {
+    Strom_regen_hex = map(Regen_on, 0, 438, 0x00, 0x7F);
+  }
+  else {
+    Strom_regen_hex = map(Regen_off, 0, 438, 0x00, 0x7F);
+  }
+  if (Strom_regen_hex_alt != Strom_regen_hex) {
     Serial.write(byte(0x83));   //Regen Current Limit
     Serial.write(byte(Strom_regen_hex));
+    digitalWrite(Regenerativbremsen_PIN_Leuchte, Regenerativbremsen);
+  }
+
+  if (Regen_on == Regen_on) { //wenn dass der Fall ist, sind die Werte immer die selben, dann würde die LED nicht umschalten 
     digitalWrite(Regenerativbremsen_PIN_Leuchte, Regenerativbremsen);
   }
 }
