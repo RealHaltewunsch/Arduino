85aea77b5b0302e28f3b86d8b0b08436534b9d92
RealHaltewunsch <61188868+RealHaltewunsch@users.noreply.github.com>
2021-02-10 13:03:28 +0100

    Logik angepasst

--- Kart/Motorsteuerung_AVR/Motorsteuerung_Initialwerte_schreiben.ino
+++ Kart/Motorsteuerung_AVR/Motorsteuerung_Initialwerte_schreiben.ino
@@ -1,12 +1,5 @@
 void Initialwerte_schreiben () {
-  Regenerativbremsen = !digitalRead(Regenerativbremsen_PIN);
-  if (Regenerativbremsen) {
-    Strom_regen_hex = map(Regen_on, 0, 438, 0x00, 0x7F);
-  }
-  else {
-    Strom_regen_hex = map(Regen_off, 0, 438, 0x00, 0x7F);
-  }
-  Serial.write(byte(0x83));   //Regen Current Limit
-  Serial.write(byte(Strom_regen_hex));
-
+  Strom_hex = 0x00;
+  Strom_regen_hex = 0x00;
+  Schalter();
 }
