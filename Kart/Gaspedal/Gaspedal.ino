0b104b7689634e63ad76f12e90d792639a1b9f2e
RealHaltewunsch <61188868+RealHaltewunsch@users.noreply.github.com>
2021-02-11 18:02:37 +0100

    Serial only

--- Kart/Gaspedal/Gaspedal.ino
+++ Kart/Gaspedal/Gaspedal.ino
@@ -1,4 +1,3 @@
-#include "ssd1306.h"
 #include<ADS1115_WE.h>
 #include<Wire.h>
 int Sollwert_analog;
@@ -10,8 +9,7 @@ ADS1115_WE adc(I2C_ADDRESS);
 
 void setup() {
   // put your setup code here, to run once:
-ssd1306_128x64_i2c_init();
-  ssd1306_clearScreen();
+
   Wire.begin();
   adc.init();
   adc.setVoltageRange_mV(ADS1115_RANGE_6144);   //maximal 5000 mV
@@ -19,13 +17,14 @@ ssd1306_128x64_i2c_init();
   adc.setVoltageRange_mV(ADS1115_RANGE_6144);
   adc.setMeasureMode(ADS1115_SINGLE);
   adc.setVoltageRange_mV(ADS1115_RANGE_6144);
+Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
   Sollwert_analog = readChannel(ADS1115_COMP_0_GND);
-  OLED_Display();
+  Serial.println(Sollwert_analog);
   delay(1000);
 }
 
@@ -38,16 +37,4 @@ int readChannel(ADS1115_MUX channel) {
   while (adc.isBusy()) {}
   voltage = adc.getResult_mV(); // alternative: getResult_mV for Millivolt
   return voltage;
-}
-
-
-void OLED_Display() {
-   ssd1306_clearScreen();
-  char Gaspedal_char[7];
-  sprintf(Gaspedal_char, "%d", Sollwert_analog);
- 
-  ssd1306_setFixedFont(ssd1306xled_font6x8);  //5x7, 6x8
-  ssd1306_setFixedFont(ssd1306xled_font8x16);
-  ssd1306_printFixed2x(10,  10, Gaspedal_char, STYLE_BOLD);
-
 }
