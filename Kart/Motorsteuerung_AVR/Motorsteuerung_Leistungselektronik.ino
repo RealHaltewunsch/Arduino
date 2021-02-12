c580b13668a10e3c826c0700ae92693b7015a515
RealHaltewunsch <61188868+RealHaltewunsch@users.noreply.github.com>
2021-02-12 16:31:21 +0100

    Leistungselektronik einschalten erkennen
    Diese Funktion dient dazu, zu erkennen, dass die Leistungselektronik eingeschaltet wird. Wenn der PIN HIGH wird, wird 3s gewartet, und dann werden die Einstellungen zur Leistungselektronik gesendet.

--- Kart/Motorsteuerung_AVR/Motorsteuerung_Leistungselektronik.ino
+++ Kart/Motorsteuerung_AVR/Motorsteuerung_Leistungselektronik.ino
@@ -1,6 +1,6 @@
 void Leistungselektronik_auslesen () {  //diese Funktion dient dazu, zu erkennen, dass die Leistungselektronik eingeschaltet wird. Wenn der PIN HIGH wird, wird 3s gewartet, und dann werden die Einstellungen zur Leistungselektronik gesendet.
   if (!Leistungselektronik_start && digitalRead(Leistungselektronik_PIN)) {
-    previousMillis_Leistungselektronik = currentMillis();
+    previousMillis_Leistungselektronik = currentMillis;
     Leistungselektronik_check = true;
   }
 
