d800378f8163371ac44ec7cf139928c534f3ae5c
RealHaltewunsch <61188868+RealHaltewunsch@users.noreply.github.com>
2021-02-14 22:33:21 +0100

    Leistungselektronik erkennen...

--- Kart/Motorsteuerung_AVR/Motorsteuerung_Freigabe_Check.ino
+++ Kart/Motorsteuerung_AVR/Motorsteuerung_Freigabe_Check.ino
@@ -1,10 +1,9 @@
 void Freigabe_Check() {
-  if (!Uebertemperatur && !Untertemperatur && !AnalogSensorFehler && !Temperatursensor_Fehler && Gaspedal_angeschlossen) {
+  if (!Uebertemperatur && !Untertemperatur && !AnalogSensorFehler && !Temperatursensor_Fehler && Gaspedal_angeschlossen && Leistungselektronik) {
     Freigabe = true;
   }
   else {
     Freigabe = false;
   }
   digitalWrite(Freigabe_PIN_Leuchte, Freigabe);
-  
 }
