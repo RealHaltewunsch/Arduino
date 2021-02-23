f755bc7626256118806224162073ee019166c490
RealHaltewunsch <61188868+RealHaltewunsch@users.noreply.github.com>
2021-02-23 22:00:13 +0100

    Rückwärtsgang Logik hinzugefügt

--- Kart/Motorsteuerung_AVR/Motorsteuerung_Schalter.ino
+++ Kart/Motorsteuerung_AVR/Motorsteuerung_Schalter.ino
@@ -4,4 +4,15 @@ void Schalter () {
   Regenerativbremsen_auslesen();
   Leistungselektronik_auslesen();
   Freigabe_Check();
+  if (Gang_wechseln) {
+    if (Gang_wechseln_delay) {        //bei diesem Durchlauf wird die Ausgangssituation hergestellt
+      Gang_Wechsel();
+      Gang_wechseln_delay = false;
+    }
+    else {
+      Serial.write(byte(0x8A));    //Direction
+      Serial.write(byte(0x00));    // STOP
+      Gang_wechseln_delay = true;  //beim nächsten Durchlauf wird der Gang gewechselt
+    }
+  }
 }
