684b97924ab319f53e38b0cefe0b010872e0a9f5
RealHaltewunsch <61188868+RealHaltewunsch@users.noreply.github.com>
2021-02-24 22:48:34 +0100

    Sportmodus im Rückwärtsgang unterbinden

--- Kart/Motorsteuerung_AVR/Motorsteuerung_Sportmodus_Notbetrieb.ino
+++ Kart/Motorsteuerung_AVR/Motorsteuerung_Sportmodus_Notbetrieb.ino
@@ -1,20 +1,25 @@
 void Sport_Modus_auslesen() {
   int Strom_hex_alt = Strom_hex;
-  bool Sportmodus_state = digitalRead(Sportmodus_PIN);
-  if (Notbetrieb && !Sportmodus_state) { //Wenn PIN HIGH dann kein Sport Modus
-    Sport_Modus = true;
-    Strom_hex = map(MAX_VALUE_CURRENT_SPORT, 0, 434, 0x00, 0x7F);
-  }
-  else if (Notbetrieb && Sportmodus_state) {
-    Sport_Modus = false;
-    Strom_hex = map(MAX_VALUE_CURRENT_LOW, 0, 434, 0x00, 0x7F);   //Sportmodus deaktiviert, Notbetrieb aus
+  if (!Rueckwaertsgang) {
+    bool Sportmodus_state = digitalRead(Sportmodus_PIN);
+    if (Notbetrieb && !Sportmodus_state) { //Wenn PIN HIGH dann kein Sport Modus
+      Sport_Modus = true;
+      Strom_hex = map(MAX_VALUE_CURRENT_SPORT, 0, 434, 0x00, 0x7F);
+    }
+    else if (Notbetrieb && Sportmodus_state) {
+      Sport_Modus = false;
+      Strom_hex = map(MAX_VALUE_CURRENT_LOW, 0, 434, 0x00, 0x7F);   //Sportmodus deaktiviert, Notbetrieb aus
+    }
+    else {
+      Sport_Modus = false;  //Sportmodus wird false, weil Notbetrieb aktiviert ist, anderen beiden Bedingungen treffen nicht zu, Frequenz wird von der ISR Notbetrieb_auslesen verändert
+      Strom_hex = map(MAX_VALUE_CURRENT_NOTBETRIEB, 0, 434, 0x00, 0x7F);
+    }
   }
   else {
-    Sport_Modus = false;  //Sportmodus wird false, weil Notbetrieb aktiviert ist, anderen beiden Bedingungen treffen nicht zu, Frequenz wird von der ISR Notbetrieb_auslesen verändert
-    Strom_hex = map(MAX_VALUE_CURRENT_NOTBETRIEB, 0, 434, 0x00, 0x7F);
+    Sport_Modus = false;
   }
   digitalWrite(Sport_Modus_PIN_Leuchte, Sport_Modus);
-  if (Strom_hex_alt != Strom_hex ) {
+  if (Strom_hex_alt != Strom_hex && !Rueckwaertsgang) {
     Serial.write(byte(0x82));   //Current Limit
     Serial.write(byte(Strom_hex));
   }
