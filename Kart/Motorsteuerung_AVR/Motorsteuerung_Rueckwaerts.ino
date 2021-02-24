e11467a7e1088cfa6aec33ebba11c7fe0dfc51bf
RealHaltewunsch <61188868+RealHaltewunsch@users.noreply.github.com>
2021-02-24 22:48:09 +0100

    Strom für Rückwärtsgang angepasst

--- Kart/Motorsteuerung_AVR/Motorsteuerung_Rueckwaerts.ino
+++ Kart/Motorsteuerung_AVR/Motorsteuerung_Rueckwaerts.ino
@@ -18,11 +18,15 @@ void Gang_Wechsel() {
   Gang_wechseln = false;
   if (Rueckwaertsgang) {
     digitalWrite(Spule_Vorwaerts, LOW);
+    Strom_hex = map(MAX_VALUE_CURRENT_RUECKWAERTS, 0, 434, 0x00, 0x7F);
+    Serial.write(byte(0x82));   //Current Limit
+    Serial.write(byte(Strom_hex));
     delay (250);
     digitalWrite(Spule_Rueckwaerts, HIGH);
   }
   else {
     digitalWrite(Spule_Rueckwaerts, LOW);
+    Sport_Modus_auslesen();
     delay (250);
     digitalWrite(Spule_Vorwaerts, HIGH);
   }
