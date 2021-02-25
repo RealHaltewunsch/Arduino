6457e5b1fa66416ee35886fe7c0c7a3e052228f4
RealHaltewunsch <61188868+RealHaltewunsch@users.noreply.github.com>
2021-02-25 22:51:37 +0100

    Bei kritischem Fehler nicht mehr Gang wechseln und abschalten

--- Kart/Motorsteuerung_AVR/Motorsteuerung_Rueckwaerts.ino
+++ Kart/Motorsteuerung_AVR/Motorsteuerung_Rueckwaerts.ino
@@ -10,19 +10,24 @@ void Rueckwaerts_auslesen() {
 
 
 void Gang_Wechsel() {
-  Gang_wechseln = false;
-  if (Rueckwaertsgang) {
+  if (Rueckwaertsgang && State != 0) {
     digitalWrite(Spule_Vorwaerts, LOW);
     Strom_hex = map(MAX_VALUE_CURRENT_RUECKWAERTS, 0, 434, 0x00, 0x7F);
     Serial.write(byte(0x82));   //Current Limit
     Serial.write(byte(Strom_hex));
     delay (250);
     digitalWrite(Spule_Rueckwaerts, HIGH);
+    Gang_wechseln = false;
   }
-  else {
+  else if (!Rueckwaertsgang && State != 0) {
     digitalWrite(Spule_Rueckwaerts, LOW);
     Sport_Modus_auslesen();
     delay (250);
+    digitalWrite(Spule_Vorwaerts, HIGH);
+    Gang_wechseln = false;
+  }
+  else if (State == 0) {
+    digitalWrite(Spule_Rueckwaerts, HIGH);
     digitalWrite(Spule_Vorwaerts, HIGH);
   }
 }
