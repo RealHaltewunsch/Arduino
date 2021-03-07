5207b8cdee2f726e31facdb7ae969954ee0f6de1
RealHaltewunsch <61188868+RealHaltewunsch@users.noreply.github.com>
2021-03-07 15:02:34 +0100

    Strom Rückwärtsgang etwas erhöht

--- Kart/Motorsteuerung_AVR/Motorsteuerung_AVR.ino
+++ Kart/Motorsteuerung_AVR/Motorsteuerung_AVR.ino
@@ -59,7 +59,7 @@
 #define MAX_VALUE_CURRENT_SPORT 300 //Ampere
 #define MAX_VALUE_CURRENT_LOW 80 //Ampere
 #define MAX_VALUE_CURRENT_NOTBETRIEB 60 //Ampere
-#define MAX_VALUE_CURRENT_RUECKWAERTS 20//Ampere
+#define MAX_VALUE_CURRENT_RUECKWAERTS 30//Ampere
 #define Regen_on 90  //Ampere
 #define Regen_on_Sport 125  //Ampere
 #define Regen_off  40   //Mindestens 10A, da sonst der Motor nicht stoppt
